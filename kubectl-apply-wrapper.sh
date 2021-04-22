2aa09a1d6b6832fece7b60c01d439a32c19a20b5
Ivan Cvitkovic <icvitkovic@gmail.com>
2021-04-21 23:59:52 -0700

    Add metallb

--- kubectl-apply-wrapper.sh
+++ kubectl-apply-wrapper.sh
@@ -26,3 +26,12 @@ echo Installing k8s dashboard...
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.4/aio/deploy/recommended.yaml
 kubectl apply -f "$MANIFEST_DIR"/dashboard
 
+echo Installing load-balancer...
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
+
+# On first install only
+if [ -z "$(kubectl get secret generic -n metallb-system memberlist --ignore-not-found)" ]; then
+    kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+fi
+kubectl apply -f "$MANIFEST_DIR"/load-balancer
